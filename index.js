5aedbad639d6607b0a28f149e32b6e428ddc9612
xxIrynaxx <irakalachenko2006@gmail.com>
2024-07-29 10:43:38 +0300

    Users blogs

--- index.js
+++ index.js
@@ -5,26 +5,6 @@
  * @return {promise}
  */
 
-// const getUsersBlogs = async users => {
-//   const blogLinks = [];
-
-//   try {
-//     const promises = users.map(async user => {
-//       const response = await fetch(`https://api.github.com/users/${user}`);
-//       const data = await response.json();
-//       return data.blog;
-//     });
-
-//     const resolvedLinks = await Promise.all(promises);
-
-//     blogLinks.push(...resolvedLinks);
-//   } catch (error) {
-//     return Promise.reject(error);
-//   }
-
-//   return blogLinks;
-// };
-
 const getUserBlog = userId =>
   fetch(`https://api.github.com/users/${userId}`).then(response =>
     response.json()
